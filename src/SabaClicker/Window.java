e18123b2b543fcd5614c02b7fcc28b52f45c2aa7
hiimhere1231 <sethaaronberger@gmail.com>
2025-07-24 14:38:34 +0000

    idk if connors still playing balatro nvm he isnt

---
+++ src/SabaClicker/Window.java
@@ -0,0 +1,13 @@
+package SabaClicker;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Window {
+    public static void main(String[] args) {
+        JFrame bonitaFrames = new JFrame();
+        bonitaFrames.setVisible(true);
+        bonitaFrames.setSize(1920, 1080);
+        bonitaFrames.setTitle("Saba Clicker");
+    }
+}
